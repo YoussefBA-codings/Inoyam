ad196ceffa530924a225dadba16989c1741a06e1
Youssef <youssef.benamor@autovisual.com>
2021-04-13 12:03:40 +0200

    add userInfo

--- api/src/models/bot.js
+++ api/src/models/bot.js
@@ -1,26 +1,26 @@
 'use strict';
 const {
-  Model
+	Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class bot extends Model {
-    /**
+	class bot extends Model {
+		/**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-    }
-  };
-  bot.init({
-    nom: DataTypes.STRING,
-    url: DataTypes.STRING,
-    annonceNumber: DataTypes.INTEGER,
-    bddAnnonceNumber: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'bot',
-  });
-  return bot;
+		static associate (models) {
+			// define association here
+		}
+	}
+	bot.init({
+		nom: DataTypes.STRING,
+		url: DataTypes.STRING,
+		annonceNumber: DataTypes.INTEGER,
+		bddAnnonceNumber: DataTypes.INTEGER
+	}, {
+		sequelize,
+		modelName: 'bot'
+	});
+	return bot;
 };
